afd8aa88c33550ac5b1fc9dd5682ec3992cb366c
davi-OD <davidodie51@gmail.com>
2021-07-28 01:24:18 +0300

    Added products but error detected

--- products/views.py
+++ products/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World')
+    products = Product.object.all()
+    return render(request, 'index.html', {'products': products})
 
 
 def new_products(request):
